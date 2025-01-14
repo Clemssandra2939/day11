a88bc5dde65ae722d2f7f4ad6db8aa96413e13a9
Clemssandra2939 <sclems600@gmail.com>
2025-01-14 04:50:48 +0100

    Explaining the rule f black jack:But the jack,Queen and King each count as 10 and the other special card is the Ace

--- Blackjack_program_Requirements_and_Game_Rule.py
+++ Blackjack_program_Requirements_and_Game_Rule.py
@@ -1,6 +1,10 @@
-# Explaing the rule f black jack
+# Explaining the rule f black jack
 
 # Blackjack Program Requirements and Game Rules
 # Blackjack is a game that is played using cards
 # The goal of the  game is to add up your cards to the largest number without going over 21
 # If the cards in yur hand add up  to more than 21,then it's called a bust and it means that you lose immediately
+# Now the way the cards is counted is that al the cards from 2 to 10 count as their face value
+# A 2 is a 2,6 is a 6 etc
+# But the jack,Queen and King each count as 10 and the other special card is the Ace
+# Ace can count as 1 or 11 depending if you have gone over 21 or if you are under 21 
