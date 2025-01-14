31c9106eb7d3a9b3c398eb5dd834762036517a8e
Clemssandra2939 <sclems600@gmail.com>
2025-01-14 04:43:01 +0100

    Explaing the rule f black jack:If the cards in yur hand add up  to more than 21,then it's called a bust and it means that you lose immediately

--- Blackjack_program_Requirements_and_Game_Rule.py
+++ Blackjack_program_Requirements_and_Game_Rule.py
@@ -1,3 +1,6 @@
+# Explaing the rule f black jack
+
 # Blackjack Program Requirements and Game Rules
 # Blackjack is a game that is played using cards
 # The goal of the  game is to add up your cards to the largest number without going over 21
+# If the cards in yur hand add up  to more than 21,then it's called a bust and it means that you lose immediately
