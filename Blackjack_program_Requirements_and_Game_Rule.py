33e0ca3256901f044ce69b89dfdccc798e8d3785
Clemssandra2939 <sclems600@gmail.com>
2025-01-14 04:37:20 +0100

    Explaining the rules of black jack:The goal of the  game is to add up your cards to the largest number without going over 21

--- Blackjack_program_Requirements_and_Game_Rule.py
+++ Blackjack_program_Requirements_and_Game_Rule.py
@@ -1,1 +1,3 @@
 # Blackjack Program Requirements and Game Rules
+# Blackjack is a game that is played using cards
+# The goal of the  game is to add up your cards to the largest number without going over 21
