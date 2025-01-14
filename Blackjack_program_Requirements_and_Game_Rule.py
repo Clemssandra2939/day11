60017de8c282f5a9d02da4e109fa724cd14d0c79
Clemssandra2939 <sclems600@gmail.com>
2025-01-14 04:52:09 +0100

    Explaining the rule of black jack:Ace can count as 1 or 11 depending if you have gone over 21 or if you are under 21

--- Blackjack_program_Requirements_and_Game_Rule.py
+++ Blackjack_program_Requirements_and_Game_Rule.py
@@ -1,4 +1,4 @@
-# Explaining the rule f black jack
+# Explaining the rule of black jack:
 
 # Blackjack Program Requirements and Game Rules
 # Blackjack is a game that is played using cards
@@ -8,3 +8,4 @@
 # A 2 is a 2,6 is a 6 etc
 # But the jack,Queen and King each count as 10 and the other special card is the Ace
 # Ace can count as 1 or 11 depending if you have gone over 21 or if you are under 21 
+
